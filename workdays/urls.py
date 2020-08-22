84770725a2a6bc922e010770739f0ddbdc8b590b
kentran304 <tranmr@gmail.com>
2020-08-22 13:26:25 +1000

    Adding routes for singup and login

--- workdays/urls.py
+++ workdays/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'workdays'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('farmerportal/', views.farmerportal, name='farmerportal'),
+    path('workerportal/', views.workerportal, name='workerportal'),
 ]
